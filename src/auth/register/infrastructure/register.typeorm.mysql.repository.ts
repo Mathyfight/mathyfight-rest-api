9780c6dac85f2a1cdc49c2b882a4d4f5bc6a1494
Raigiku <raigiku@hotmail.com>
2021-06-21 14:51:26 -0500

    Register now registers player and avatar

--- src/auth/register/infrastructure/register.typeorm.mysql.repository.ts
+++ src/auth/register/infrastructure/register.typeorm.mysql.repository.ts
@@ -1,6 +1,8 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Email } from 'src/auth/core/domain/value-object/email';
 import { Username } from 'src/auth/core/domain/value-object/username';
+import { AvatarTypeOrmMySql } from 'src/database/typeorm/mysql/entity/avatar.typeorm.mysql';
+import { PlayerTypeOrmMySql } from 'src/database/typeorm/mysql/entity/player.typeorm.mysql';
 import { UserTypeOrmMySql } from 'src/database/typeorm/mysql/entity/user.typeorm.mysql';
 import { Uuid } from 'src/shared/domain/value-object/general/uuid';
 import { Repository } from 'typeorm';
@@ -11,6 +13,10 @@ export class RegisterTypeOrmMySqlRepository implements RegisterRepository {
   constructor(
     @InjectRepository(UserTypeOrmMySql)
     readonly userRepository: Repository<UserTypeOrmMySql>,
+    @InjectRepository(PlayerTypeOrmMySql)
+    readonly playerRepository: Repository<PlayerTypeOrmMySql>,
+    @InjectRepository(AvatarTypeOrmMySql)
+    readonly avatarRepository: Repository<AvatarTypeOrmMySql>,
   ) {}
 
   async getOneUserIdByUsername(username: Username): Promise<Uuid | null> {
@@ -35,6 +41,23 @@ export class RegisterTypeOrmMySqlRepository implements RegisterRepository {
       email: user.email.val,
       hashedPassword: user.hashedPassword.val,
       username: user.username.val,
+    });
+    await this.playerRepository.save({
+      id: user.player.id.val,
+      gold: user.player.gold,
+      user: { id: user.id.val },
+    });
+    await this.avatarRepository.save({
+      id: user.player.avatar.id.val,
+      name: user.player.avatar.name,
+      currentExperience: user.player.avatar.currentExperience,
+      color: user.player.avatar.color,
+      attack: user.player.avatar.attack,
+      defense: user.player.avatar.defense,
+      health: user.player.avatar.health,
+      level: user.player.avatar.level,
+      race: user.player.avatar.race,
+      player: { id: user.player.id.val },
     });
   }
 }
