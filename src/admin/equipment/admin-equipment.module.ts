11eb56642052df7d7b5d5cf1c5b2dda09c077776
Raigiku <raigiku@hotmail.com>
2021-08-31 00:43:11 -0500

    Change module file name

--- src/admin/equipment/admin-equipment.module.ts
+++ src/admin/equipment/admin-equipment.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AdminAddEquipmentsModule } from './add-equipment/add-equipment.module';
-import { AdminGetEquipmentsModule } from './get-equipments/admin-get-equipments.module';
+import { AdminAddEquipmentsModule } from './admin-add-equipment/admin-add-equipment.module';
+import { AdminGetEquipmentsModule } from './admin-get-equipments/admin-get-equipments.module';
 
 @Module({
   imports: [AdminGetEquipmentsModule, AdminAddEquipmentsModule],
