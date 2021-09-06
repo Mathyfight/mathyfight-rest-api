f54b8c9ae8bc50c5af3ec47834a9f4c2e353559c
Raigiku <raigiku@hotmail.com>
2021-09-06 17:40:52 -0500

    Fix player stats query

--- src/player/get-player-statistics/infrastructure/get-player-statistics.typeorm.mysql.repository.ts
+++ src/player/get-player-statistics/infrastructure/get-player-statistics.typeorm.mysql.repository.ts
@@ -35,32 +35,30 @@ export class GetPlayerStatisticsTypeOrmMySqlRepository
       readonly battles: string;
     } = (
       await this.battleRepository.query(
-        `
-        select
-          count(*) as battles,
-          sum(a.abandoned = 0 and a.enemy_defense = 0 and a.enemy_health = 0) as victories,
-          sum(a.abandoned = 1 or (a.avatar_defense = 0 and a.avatar_health = 0)) as defeats
-        from (
-          select
-            b.id,
-            b.abandoned,
-            b.enemy_defense,
-            b.enemy_health,
-            b.avatar_defense,
-            b.avatar_health
-          from
-            battle b
-          join player_unlocked_math_topic_level pumtl on
-            pumtl.id = b.player_unlocked_math_topic_level_id
-          join player p on
-            p.id = pumtl.player_id
-          join \`user\` u on
-            u.id = p.user_id
-          where
-            u.id = ?
-            and (b.enemy_defense = 0 and b.enemy_health = 0) or (b.avatar_defense = 0 and b.avatar_health = 0) or (b.abandoned = 1)
-        ) as a;
-      `,
+        `select
+    count(*) as battles,
+    coalesce(sum(a.abandoned = 0 and a.enemy_defense = 0 and a.enemy_health = 0), 0) as victories,
+    coalesce(sum(a.abandoned = 1 or (a.avatar_defense = 0 and a.avatar_health = 0)), 0) as defeats
+  from
+    (
+    select
+      b.id,
+      b.abandoned,
+      b.enemy_defense,
+      b.enemy_health,
+      b.avatar_defense,
+      b.avatar_health
+    from
+      battle b
+    join player_unlocked_math_topic_level pumtl on
+      pumtl.id = b.player_unlocked_math_topic_level_id
+    join player p on
+      p.id = pumtl.player_id
+    join \`user\` u on
+      u.id = p.user_id
+    where
+      u.id = ? and ((b.enemy_defense = 0 and b.enemy_health = 0) or (b.avatar_defense = 0 and b.avatar_health = 0) or (b.abandoned = 1))
+          ) as a;`,
         [userId],
       )
     )[0];
@@ -90,42 +88,47 @@ export class GetPlayerStatisticsTypeOrmMySqlRepository
       readonly advancedFailed: string;
     } = (
       await this.battleRepository.query(
-        `
-        select
-          count(*) as total,
-          sum(a.solved = 1) as solved,
-          sum(a.solved = 0) as failed,
-          sum(a.difficulty_id = '64348b20-6e73-4b10-804e-cf212565ac73') as easyTotal,
-          sum(a.difficulty_id = '64348b20-6e73-4b10-804e-cf212565ac73' and a.solved = 1) as easySolved,
-          sum(a.difficulty_id = '64348b20-6e73-4b10-804e-cf212565ac73' and a.solved = 0) as easyFailed,
-          sum(a.difficulty_id = '3b719e85-3eb5-46ba-b641-e21018299b77') as intermediateTotal,
-          sum(a.difficulty_id = '3b719e85-3eb5-46ba-b641-e21018299b77' and a.solved = 1) as intermediateSolved,
-          sum(a.difficulty_id = '3b719e85-3eb5-46ba-b641-e21018299b77' and a.solved = 0) as intermediateFailed,
-          sum(a.difficulty_id = 'bce74b84-995f-49d9-acf3-798cf8847682') as advancedTotal,
-          sum(a.difficulty_id = 'bce74b84-995f-49d9-acf3-798cf8847682' and a.solved = 1) as advancedSolved,
-          sum(a.difficulty_id = 'bce74b84-995f-49d9-acf3-798cf8847682' and a.solved = 0) as advancedFailed
-        from (
-          select 
-            bmp.id, 
-            bmp.solved,
-            b.abandoned,
-            b.enemy_defense,
-            b.enemy_health,
-            b.avatar_defense,
-            b.avatar_health,
-            d.id as difficulty_id
-          from battle_math_problem bmp 
-          join battle b on b.id =bmp.battle_id 
-          join player_unlocked_math_topic_level pumtl on pumtl.id =b.player_unlocked_math_topic_level_id 
-          join player p on p.id = pumtl.player_id 
-          join \`user\` u on u.id = p.user_id 
-          join math_problem mp on mp.id =bmp.math_problem_id 
-          join difficulty d on d.id =mp.difficulty_id 
-          where
-            u.id = ?
-            and (b.enemy_defense = 0 and b.enemy_health = 0) or (b.avatar_defense = 0 and b.avatar_health = 0) or (b.abandoned = 1)
-        ) as a;
-      `,
+        `select
+      count(*) as total,
+      coalesce(sum(a.solved = 1),0) as solved,
+      coalesce(sum(a.solved = 0),0) as failed,
+      coalesce(sum(a.difficulty_id = '64348b20-6e73-4b10-804e-cf212565ac73'),0) as easyTotal,
+      coalesce(sum(a.difficulty_id = '64348b20-6e73-4b10-804e-cf212565ac73' and a.solved = 1),0) as easySolved,
+      coalesce(sum(a.difficulty_id = '64348b20-6e73-4b10-804e-cf212565ac73' and a.solved = 0),0) as easyFailed,
+      coalesce(sum(a.difficulty_id = '3b719e85-3eb5-46ba-b641-e21018299b77'),0) as intermediateTotal,
+      coalesce(sum(a.difficulty_id = '3b719e85-3eb5-46ba-b641-e21018299b77' and a.solved = 1),0) as intermediateSolved,
+      coalesce(sum(a.difficulty_id = '3b719e85-3eb5-46ba-b641-e21018299b77' and a.solved = 0),0) as intermediateFailed,
+      coalesce(sum(a.difficulty_id = 'bce74b84-995f-49d9-acf3-798cf8847682'),0) as advancedTotal,
+      coalesce(sum(a.difficulty_id = 'bce74b84-995f-49d9-acf3-798cf8847682' and a.solved = 1),0) as advancedSolved,
+      coalesce(sum(a.difficulty_id = 'bce74b84-995f-49d9-acf3-798cf8847682' and a.solved = 0),0) as advancedFailed
+    from
+      (
+      select
+        bmp.id,
+        bmp.solved,
+        b.abandoned,
+        b.enemy_defense,
+        b.enemy_health,
+        b.avatar_defense,
+        b.avatar_health,
+        d.id as difficulty_id
+      from
+        battle_math_problem bmp
+      join battle b on
+        b.id = bmp.battle_id
+      join player_unlocked_math_topic_level pumtl on
+        pumtl.id = b.player_unlocked_math_topic_level_id
+      join player p on
+        p.id = pumtl.player_id
+      join \`user\` u on
+        u.id = p.user_id
+      join math_problem mp on
+        mp.id = bmp.math_problem_id
+      join difficulty d on
+        d.id = mp.difficulty_id
+      where
+        u.id = ? and ((b.enemy_defense = 0 and b.enemy_health = 0) or (b.avatar_defense = 0 and b.avatar_health = 0) or (b.abandoned = 1))
+            ) as a;`,
         [userId],
       )
     )[0];
@@ -166,38 +169,63 @@ export class GetPlayerStatisticsTypeOrmMySqlRepository
         readonly failed: string;
       },
     ] = await this.battleRepository.query(
-      `
-        select 
-          a.math_area_id as areaId,
-          a.math_area_name as name,
-          count(a.bmp_id) as total,
-          sum(a.solved = 1) as solved,
-          sum(a.solved = 0) as failed
-        from (
-          select 
-            bmp.id as bmp_id, 
-            bmp.solved,
-            b.abandoned,
-            b.enemy_defense,
-            b.enemy_health,
-            b.avatar_defense,
-            b.avatar_health,
-            ma.name as math_area_name,
-            ma.id as math_area_id
-          from battle_math_problem bmp 
-          join battle b on b.id =bmp.battle_id 
-          join player_unlocked_math_topic_level pumtl on pumtl.id =b.player_unlocked_math_topic_level_id 
-          join player p on p.id = pumtl.player_id 
-          join \`user\` u on u.id = p.user_id 
-          join math_problem mp on mp.id =bmp.math_problem_id 
-          join math_topic mt on mt.id =mp.math_topic_id 
-          join math_area ma on ma.id =mt.math_area_id 
-          where
-            u.id = ?
-            and (b.enemy_defense = 0 and b.enemy_health = 0) or (b.avatar_defense = 0 and b.avatar_health = 0) or (b.abandoned = 1)
-        ) as a
-        group by a.math_area_id;
-      `,
+      `select 
+    ma.id as areaId,
+    ma.name as name,
+    coalesce(b.total, 0) as total,
+    coalesce(b.solved, 0) as solved,
+    coalesce(b.failed, 0) as failed
+  from
+    math_area ma
+  left join 
+  (
+    select
+      a.math_area_id as areaId,
+      a.math_area_name as name,
+      count(a.bmp_id) as total,
+      sum(a.solved = 1) as solved,
+      sum(a.solved = 0) as failed
+    from
+      (
+      select
+        bmp.id as bmp_id,
+        bmp.solved,
+        b.abandoned,
+        b.enemy_defense,
+        b.enemy_health,
+        b.avatar_defense,
+        b.avatar_health,
+        ma.name as math_area_name,
+        ma.id as math_area_id
+      from
+        battle_math_problem bmp
+      join battle b on
+        b.id = bmp.battle_id
+      join player_unlocked_math_topic_level pumtl on
+        pumtl.id = b.player_unlocked_math_topic_level_id
+      join player p on
+        p.id = pumtl.player_id
+      join \`user\` u on
+        u.id = p.user_id
+      join math_problem mp on
+        mp.id = bmp.math_problem_id
+      join math_topic mt on
+        mt.id = mp.math_topic_id
+      join math_area ma on
+        ma.id = mt.math_area_id
+      where
+        u.id = ?
+        and ((b.enemy_defense = 0
+          and b.enemy_health = 0)
+        or (b.avatar_defense = 0
+          and b.avatar_health = 0)
+        or (b.abandoned = 1))
+          ) as a
+    group by
+      a.math_area_id
+    ) as b
+    on
+    ma.id = b.areaId;`,
       [userId],
     );
 
@@ -226,39 +254,63 @@ export class GetPlayerStatisticsTypeOrmMySqlRepository
         readonly failed: string;
       },
     ] = await this.battleRepository.query(
-      `
-        select 
-          a.math_topic_id as topicId,
-          a.math_area_id as areaId,
-          a.math_topic_name as name,
-          count(a.bmp_id) as total,
-          sum(a.solved = 1) as solved,
-          sum(a.solved = 0) as failed
-        from (
-          select 
-            bmp.id as bmp_id, 
-            bmp.solved,
-            b.abandoned,
-            b.enemy_defense,
-            b.enemy_health,
-            b.avatar_defense,
-            b.avatar_health,
-            mt.name as math_topic_name,
-            mt.id as math_topic_id,
-            mt.math_area_id  as math_area_id
-          from battle_math_problem bmp 
-          join battle b on b.id =bmp.battle_id 
-          join player_unlocked_math_topic_level pumtl on pumtl.id =b.player_unlocked_math_topic_level_id 
-          join player p on p.id = pumtl.player_id 
-          join \`user\` u on u.id = p.user_id 
-          join math_problem mp on mp.id =bmp.math_problem_id 
-          join math_topic mt on mt.id =mp.math_topic_id
-          where
-            u.id = ?
-            and (b.enemy_defense = 0 and b.enemy_health = 0) or (b.avatar_defense = 0 and b.avatar_health = 0) or (b.abandoned = 1)
-        ) as a
-        group by a.math_topic_id;
-      `,
+      `select 
+    mt.id as topicId,
+    mt.math_area_id as areaId,
+    mt.name as name,
+    coalesce(b.total, 0) as total,
+    coalesce(b.solved, 0) as solved,
+    coalesce(b.failed, 0) as failed
+  from
+    math_topic mt
+  left join (
+    select
+      a.math_topic_id as topicId,
+      a.math_area_id as areaId,
+      a.math_topic_name as name,
+      count(a.bmp_id) as total,
+      sum(a.solved = 1) as solved,
+      sum(a.solved = 0) as failed
+    from
+      (
+      select
+        bmp.id as bmp_id,
+        bmp.solved,
+        b.abandoned,
+        b.enemy_defense,
+        b.enemy_health,
+        b.avatar_defense,
+        b.avatar_health,
+        mt.name as math_topic_name,
+        mt.id as math_topic_id,
+        mt.math_area_id as math_area_id
+      from
+        battle_math_problem bmp
+      join battle b on
+        b.id = bmp.battle_id
+      join player_unlocked_math_topic_level pumtl on
+        pumtl.id = b.player_unlocked_math_topic_level_id
+      join player p on
+        p.id = pumtl.player_id
+      join \`user\` u on
+        u.id = p.user_id
+      join math_problem mp on
+        mp.id = bmp.math_problem_id
+      join math_topic mt on
+        mt.id = mp.math_topic_id
+      where
+        u.id = ?
+        and ((b.enemy_defense = 0
+          and b.enemy_health = 0)
+        or (b.avatar_defense = 0
+          and b.avatar_health = 0)
+        or (b.abandoned = 1))
+          ) as a
+    group by
+      a.math_topic_id
+    ) as b
+  on
+    mt.id = b.topicId;`,
       [userId],
     );
 
